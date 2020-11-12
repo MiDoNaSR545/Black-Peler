1f1658a1b110ab2344cae74478edc62036e38fb5
Francisco Franco <franciscofranco.1990@gmail.com>
2020-11-12 15:19:00 +0200

    msm: performance: prevent the driver from handling userspace boosts
    Improve Active drain & help in fixing freq stuck issue
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -56,6 +56,7 @@ static unsigned int aggr_top_load;
 /*******************************sysfs start************************************/
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
+#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -113,6 +114,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
+#endif
 
 	return 0;
 }

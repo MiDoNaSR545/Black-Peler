1c8cde1289a7eb619bb8b780394f7c939d6d7504
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-11-28 09:25:04 +0800

    fs/kernel: update manual hooks to v1.6

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -436,6 +436,16 @@ static void input_handle_event(struct input_dev *dev,
  * to 'seed' initial state of a switch or initial position of absolute
  * axis, etc.
  */
+<<<<<<< HEAD
+=======
+
+#ifdef CONFIG_KSU
+extern bool ksu_input_hook __read_mostly;
+extern __attribute__((cold)) int ksu_handle_input_handle_event(
+			unsigned int *type, unsigned int *code, int *value);
+#endif
+
+>>>>>>> 87e6c1fcbd9c (fs/kernel: update manual hooks to v1.6)
 void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {

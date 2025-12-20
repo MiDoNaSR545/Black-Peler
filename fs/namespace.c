2be784ccc9660fc2b37322bfd0fcc99f29aeb550
MiDoNaSR545 <ahmednasr1973913@gmail.com>
2025-12-20 23:05:01 +0000

    Revert "BACKPORT: fs/namespace.c path_umount [5.10.9]"
    This reverts commit ce535c55f73588d8f5a551c8b1ec88441280535d.

--- fs/namespace.c
+++ fs/namespace.c
@@ -1936,6 +1936,8 @@ static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
+	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
@@ -1949,7 +1951,6 @@ static int can_umount(const struct path *path, int flags)
 	return 0;
 }
 
-// caller is responsible for flags being sane
 int path_umount(struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);

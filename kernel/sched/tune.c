306e4701da557b1b55251d133f6b0fe6dd5904ce
MiDoNaSR545 <ahmednasr1973913@gmail.com>
2025-12-21 20:28:53 +0000

    Revert "sched: Add branch predictor hints to battery saver checks"
    This reverts commit d52ba697ed9c6aa0ba69b9e9abfb1c7a4a69e159.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -5,7 +5,6 @@
 #include <linux/printk.h>
 #include <linux/rcupdate.h>
 #include <linux/slab.h>
-#include <linux/battery_saver.h>
 
 #include <trace/events/sched.h>
 
@@ -235,7 +234,7 @@ schedtune_cpu_update(int cpu, u64 now)
 	/* The root boost group is always active */
 	boost_max = bg->group[0].boost;
 	boost_ts = now;
-	for (idx = 0; idx < BOOSTGROUPS_COUNT; ++idx) {
+	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx) {
 		/*
 		 * A boost group affects a CPU only if it has
 		 * RUNNABLE tasks on that CPU or it has hold
@@ -554,7 +553,7 @@ int schedtune_task_boost(struct task_struct *p)
 	struct schedtune *st;
 	int task_boost;
 
-	if (unlikely(!schedtune_initialized) || unlikely(is_battery_saver_on()))
+	if (unlikely(!schedtune_initialized))
 		return 0;
 
 	/* Get task boost value */
@@ -571,7 +570,7 @@ int schedtune_prefer_idle(struct task_struct *p)
 	struct schedtune *st;
 	int prefer_idle;
 
-	if (unlikely(!schedtune_initialized) || unlikely(is_battery_saver_on()))
+	if (unlikely(!schedtune_initialized))
 		return 0;
 
 	/* Get prefer_idle value */
@@ -587,9 +586,6 @@ static u64
 prefer_idle_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
-
-	if (unlikely(is_battery_saver_on()))
-		return 0;
 
 	return st->prefer_idle;
 }
@@ -608,9 +604,6 @@ static s64
 boost_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
-
-	if (unlikely(is_battery_saver_on()))
-		return 0;
 
 	return st->boost;
 }
